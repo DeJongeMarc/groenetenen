abb4a712113017f7cdbad9ab0ee54f8b9be7650f
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-12-07 15:21:45 +0100

    ANDERE APPLICATIELAGEN toegevoegd

--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import be.vdab.services.FiliaalService;
+
 @Controller
 @RequestMapping("/filialen")
 public class FiliaalController {
@@ -14,6 +16,11 @@ public class FiliaalController {
 	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
 	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
 	private static final Logger LOGGER = Logger.getLogger(FiliaalController.class.getName());
+	private final FiliaalService filiaalService;
+
+	FiliaalController(FiliaalService filiaalService) {
+		this.filiaalService = filiaalService;
+	}
 
 	@GetMapping
 	String findAll() {
