07d964420f5a029c1e9874664b425685f4a7c95a
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-12-21 12:04:50 +0100

    34.5 Een DELETE request

--- src/main/java/be/vdab/restservices/FiliaalRestController.java
+++ src/main/java/be/vdab/restservices/FiliaalRestController.java
@@ -1,11 +1,17 @@
 package be.vdab.restservices;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.vdab.entities.Filiaal;
+import be.vdab.exceptions.FiliaalHeeftNogWerknemersException;
+import be.vdab.exceptions.FiliaalNietGevondenException;
 import be.vdab.services.FiliaalService;
 
 @RestController
@@ -19,7 +25,29 @@ class FiliaalRestController {
 
 	@GetMapping("{filiaal}")
 	Filiaal read(@PathVariable Filiaal filiaal) {
+		if (filiaal == null) {
+			throw new FiliaalNietGevondenException();
+		}
 		return filiaal;
+	}
+
+	@DeleteMapping("{filiaal}")
+	void delete(@PathVariable Filiaal filiaal) {
+		if (filiaal == null) {
+			throw new FiliaalNietGevondenException();
+		}
+		filiaalService.delete(filiaal.getId());
+	}
+
+	@ExceptionHandler(FiliaalNietGevondenException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	void filiaalNietGevonden() {
+	}
+
+	@ExceptionHandler(FiliaalHeeftNogWerknemersException.class)
+	@ResponseStatus(HttpStatus.CONFLICT)
+	String filiaalHeeftNogWerknemers() {
+		return "filiaal heeft nog werknemers";
 	}
 
 }
