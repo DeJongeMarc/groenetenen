53a4c064f13921afbf992650a9a739231666b9e3
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2018-01-04 13:03:05 +0100

    42.9 Principals en authorization in een database opslaan

--- src/main/java/be/vdab/security/SecurityConfig.java
+++ src/main/java/be/vdab/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package be.vdab.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,11 +16,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String MANAGER = "manager";
 	private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
 	private static final String MAGAZIJNIER = "magazijnier";
+	private static final String USERS_BY_USERNAME = "select naam as username, paswoord as password, actief as enabled from gebruikers where naam = ?";
+	private static final String AUTHORITIES_BY_USERNAME = "select gebruikers.naam as username, rollen.naam as authorities from gebruikers inner join gebruikersrollen on gebruikers.id = gebruikersrollen.gebruikerid inner join rollen on rollen.id = gebruikersrollen.rolid"
+			+ " where gebruikers.naam= ?";
+	private final DataSource dataSource;
+
+	public SecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("joe").password("theboss").authorities(MANAGER).and().withUser("averell")
-				.password("hungry").authorities(HELPDESKMEDEWERKER, MAGAZIJNIER);
+		auth.jdbcAuthentication().dataSource(dataSource)
+		  .usersByUsernameQuery(USERS_BY_USERNAME)
+		  .authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME); 
 	}
 
 	@Override
@@ -32,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.mvcMatchers("/filialen/toevoegen", "/filialen/*/wijzigen", "/filialen/*/verwijderen")
 				.hasAuthority(MANAGER).mvcMatchers(HttpMethod.POST, "/filialen").hasAuthority(MANAGER)
 				.mvcMatchers("/werknemers").hasAnyAuthority(MAGAZIJNIER, HELPDESKMEDEWERKER).mvcMatchers("/", "/login")
-				.permitAll().mvcMatchers("/**").authenticated().and().exceptionHandling().accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
+				.permitAll().mvcMatchers("/**").authenticated().and().exceptionHandling()
+				.accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
 
 	}
 
